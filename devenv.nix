b77609e8bcfdf1a894764a7f0e6e077d88a22c63
Zeno Belli <xeno@os76.xyz>
2025-09-06 10:56:48 +0200

    build(devenv.nix): add Nginx to local processes

--- devenv.nix
+++ devenv.nix
@@ -5,44 +5,125 @@
   inputs,
   ...
 }: {
-  env.GUM_FORMAT_THEME = "tokyo-night";
-  env.TEST_DIR = "tests";
-  env.CAROOT = "tests/certs";
+  env = {
+    GUM_FORMAT_THEME = "tokyo-night";
+    CAROOT = "tests/certs";
+    CGO_ENABLE = "0";
+    OS76_DOCKER_REGISTRY = "registry.0.os76.xyz";
+    OS76_DOCKER_USER = "xeno";
+  };
 
   packages = with pkgs; [
     git
     mkcert
     gum
+    goreleaser
+    curl
+    jq
+    httpie
   ];
 
-  # git-hooks.hooks.shellcheck.enable = true;
+  git-hooks = {
+    excludes = [
+      "devenv.nix"
+      "flake.nix"
+      ".gitignore"
+      ".envrc"
+    ];
+    hooks = {
+      shellcheck.enable = true;
+      end-of-file-fixer.enable = true;
+      detect-aws-credentials.enable = true;
+      detect-private-keys.enable = true;
+      ripsecrets.enable = true;
+      commitizen.enable = true;
+    };
+  };
 
   languages.go.enable = true;
 
+  services.nginx = {
+    enable = true;
+    httpConfig = ''
+      #
+      # Mozilla SSL Configuration Generator
+      #
+      # https://ssl-config.mozilla.org/#server=nginx&version=1.27.3&config=intermediate&openssl=3.4.0&guideline=5.7
+      #
+      ssl_protocols TLSv1.2 TLSv1.3;
+      ssl_ecdh_curve X25519:prime256v1:secp384r1;
+      ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
+      ssl_prefer_server_ciphers off;
+
+      # see also ssl_session_ticket_key alternative to stateful session cache
+      ssl_session_timeout 1d;
+      ssl_session_cache shared:MozSSL:10m;  # about 40000 sessions
+
+      # curl https://ssl-config.mozilla.org/ffdhe2048.txt > /path/to/dhparam
+      ssl_dhparam "${config.env.DEVENV_ROOT}/${config.env.CAROOT}/dhparam";
+      ssl_certificate ${config.env.DEVENV_ROOT}/${config.env.CAROOT}/full-cert.pem;
+      ssl_certificate_key ${config.env.DEVENV_ROOT}/${config.env.CAROOT}/key.pem;
+
+      server {
+          server_name  _;
+
+          root ${config.env.DEVENV_ROOT};
+
+          listen 9443 ssl;
+          listen [::]:9443 ssl;
+          http2 on;
+
+          location / {
+              proxy_pass       http://localhost:8080;
+          }
+      }
+
+      server {
+          server_name  example.com;
+
+          root ${config.env.DEVENV_ROOT};
+
+          listen 9444 ssl proxy_protocol;
+          listen [::]:9444 ssl proxy_protocol;
+
+          # http2 on;
+          location / {
+              proxy_pass       http://localhost:8080;
+          }
+      }
+    '';
+  };
+
   services.httpbin = {
     enable = true;
-    extraArgs = [
-      "--keyfile"
-      "tests/certs/key.pem"
-      "--certfile"
-      "tests/certs/cert.pem"
-      "--ca-certs"
-      "tests/certs/rootCA.pem"
-    ];
   };
 
   scripts.hello.exec = ''
     gum format "# Devenv shell"
   '';
 
+  scripts.goreleaser-test-release.exec = ''
+    ${pkgs.goreleaser}/bin/goreleaser release --snapshot --clean
+  '';
+
+  scripts.goreleaser-release.exec = ''
+    ${pkgs.goreleaser}/bin/goreleaser release --clean
+  '';
+
   scripts.create-certs.exec = ''
     gum format "## creating certs"
+    test -f $CAROOT/dhparam || curl https://ssl-config.mozilla.org/ffdhe2048.txt > $CAROOT/dhparam
     test -d $CAROOT || mkdir -p $CAROOT && mkcert -key-file $CAROOT/key.pem -cert-file $CAROOT/cert.pem localhost 127.0.0.1 example.com *.example.com
+    test -d $CAROOT/full-cert.pem || cat  $CAROOT/cert.pem $CAROOT/rootCA.pem > $CAROOT/full-cert.pem
   '';
 
   scripts.build.exec = ''
     gum format "## building..."
     ./build.sh
+  '';
+
+  scripts.test-curl.exec = ''
+    curl "https://localhost:9443/get" -k -v
   '';
 
   scripts.test-sample-config.exec = ''
@@ -75,6 +156,7 @@
   '';
 
   enterShell = ''
+    gum format "# Devenv shell"
     go version
     test -d $CAROOT || create-certs
   '';
