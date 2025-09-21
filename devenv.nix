1b599d7648db8ed8dae62b12c5d69dc3cd5b15ee
Zeno Belli <xeno@os76.xyz>
2025-09-21 10:45:51 +0200

    test: add tests for certinfo command

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,8 @@
   env = {
     GUM_FORMAT_THEME = "tokyo-night";
     CAROOT = "tests/certs";
+    ED25519_DIR = "tests/certs/ed25519_cert";
+    ECDSA_DIR = "tests/certs/ecdsa-cert";
     CGO_ENABLE = "0";
     OS76_DOCKER_REGISTRY = "registry.0.os76.xyz";
     OS76_DOCKER_USER = "xeno";
@@ -15,6 +17,7 @@
 
   packages = with pkgs; [
     git
+    openssl
     mkcert
     gum
     goreleaser
@@ -45,60 +48,80 @@
   services.nginx = {
     enable = true;
     httpConfig = ''
-      #
-      # Mozilla SSL Configuration Generator
-      #
-      # https://ssl-config.mozilla.org/#server=nginx&version=1.27.3&config=intermediate&openssl=3.4.0&guideline=5.7
-      #
-      ssl_protocols TLSv1.2 TLSv1.3;
-      ssl_ecdh_curve X25519:prime256v1:secp384r1;
-      ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
-      ssl_prefer_server_ciphers off;
+        #
+        # Mozilla SSL Configuration Generator
+        #
+        # https://ssl-config.mozilla.org/#server=nginx&version=1.27.3&config=intermediate&openssl=3.4.0&guideline=5.7
+        #
+        ssl_protocols TLSv1.2 TLSv1.3;
+        ssl_ecdh_curve X25519:prime256v1:secp384r1;
+        ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-CHACHA20-POLY1305;
+        ssl_prefer_server_ciphers off;
+        ssl_certificate ${config.env.DEVENV_ROOT}/${config.env.CAROOT}/full-cert.pem;
+        ssl_certificate_key ${config.env.DEVENV_ROOT}/${config.env.CAROOT}/key.pem;
 
-      # see also ssl_session_ticket_key alternative to stateful session cache
-      ssl_session_timeout 1d;
-      ssl_session_cache shared:MozSSL:10m;  # about 40000 sessions
+        # curl https://ssl-config.mozilla.org/ffdhe2048.txt > /path/to/dhparam
+        ssl_dhparam "${config.env.DEVENV_ROOT}/${config.env.CAROOT}/dhparam";
 
-      # curl https://ssl-config.mozilla.org/ffdhe2048.txt > /path/to/dhparam
-      ssl_dhparam "${config.env.DEVENV_ROOT}/${config.env.CAROOT}/dhparam";
-      ssl_certificate ${config.env.DEVENV_ROOT}/${config.env.CAROOT}/full-cert.pem;
-      ssl_certificate_key ${config.env.DEVENV_ROOT}/${config.env.CAROOT}/key.pem;
+        server {
+            server_name  _;
+            root ${config.env.DEVENV_ROOT};
+            listen 9443 ssl;
+            listen [::]:9443 ssl;
+            http2 on;
+            location / {
+                proxy_pass       http://localhost:8080;
+                proxy_set_header Host                   $host;
+                proxy_set_header X-Forwarded-For        $remote_addr;
+            }
+        }
+
+        server {
+            server_name  example.com;
+            root ${config.env.DEVENV_ROOT};
+            listen 9444 ssl proxy_protocol;
+            listen [::]:9444 ssl proxy_protocol;
+            http2 on;
+            location / {
+                proxy_pass                  http://localhost:8080;
+                proxy_pass_request_headers  on;
+                proxy_set_header            Host $host;
+                proxy_set_header            X-Proxy-Protocol        enabled;
+                proxy_set_header            X-Proxy-Protocol-Addr   $proxy_protocol_addr;
+                proxy_set_header            X-Proxy-Protocol-Port   $proxy_protocol_port;
+                proxy_set_header            X-Forwarded-For         $proxy_protocol_addr;
+            }
+        }
+
+        server {
+            server_name  _;
+            root ${config.env.DEVENV_ROOT};
+            ssl_certificate ${config.env.DEVENV_ROOT}/${config.env.ED25519_DIR}/ed25519.crt;
+            ssl_certificate_key ${config.env.DEVENV_ROOT}/${config.env.ED25519_DIR}/ed25519.key;
+            listen 9445 ssl;
+            listen [::]:9445 ssl;
+            http2 on;
+            location / {
+                proxy_pass       http://localhost:8080;
+                proxy_set_header Host                   $host;
+                proxy_set_header X-Forwarded-For        $remote_addr;
+            }
+        }
 
       server {
-          server_name  _;
-
-          root ${config.env.DEVENV_ROOT};
-
-          listen 9443 ssl;
-          listen [::]:9443 ssl;
-          http2 on;
-
-          location / {
-              proxy_pass       http://localhost:8080;
-              proxy_set_header Host $host;
-              proxy_set_header X-Forwarded-For $remote_addr;
-          }
-      }
-
-      server {
-          server_name  example.com;
-
-          root ${config.env.DEVENV_ROOT};
-
-          listen 9444 ssl proxy_protocol;
-          listen [::]:9444 ssl proxy_protocol;
-
-          # http2 on;
-          location / {
-              proxy_pass       http://localhost:8080;
-              proxy_pass_request_headers on;
-              proxy_set_header Host $host;
-              proxy_set_header X-Proxy-Protocol enabled;
-              proxy_set_header X-Proxy-Protocol-Addr       $proxy_protocol_addr;
-              proxy_set_header X-Proxy-Protocol-Port       $proxy_protocol_port;
-              proxy_set_header X-Forwarded-For $proxy_protocol_addr;
-          }
-      }
+            server_name  _;
+            root ${config.env.DEVENV_ROOT};
+            listen 9446 ssl;
+            ssl_certificate ${config.env.DEVENV_ROOT}/${config.env.ECDSA_DIR}/ecdsa.crt;
+            ssl_certificate_key ${config.env.DEVENV_ROOT}/${config.env.ECDSA_DIR}/ecdsa.key;
+            listen [::]:9446 ssl;
+            http2 on;
+            location / {
+                proxy_pass       http://localhost:8080;
+                proxy_set_header Host                   $host;
+                proxy_set_header X-Forwarded-For        $remote_addr;
+            }
+        }
     '';
   };
 
@@ -119,10 +142,25 @@
   '';
 
   scripts.create-certs.exec = ''
-    test -f $CAROOT/dhparam || curl https://ssl-config.mozilla.org/ffdhe2048.txt > $CAROOT/dhparam
     test -d $CAROOT || mkdir -p $CAROOT
-    test -d $CAROOT/cert.pem || mkcert -key-file $CAROOT/key.pem -cert-file $CAROOT/cert.pem localhost 127.0.0.1 example.com *.example.com
-    test -d $CAROOT/full-cert.pem || cat  $CAROOT/cert.pem $CAROOT/rootCA.pem > $CAROOT/full-cert.pem
+    test -f $CAROOT/dhparam || curl https://ssl-config.mozilla.org/ffdhe2048.txt > $CAROOT/dhparam
+    test -f $CAROOT/cert.pem || mkcert -key-file $CAROOT/key.pem -cert-file $CAROOT/cert.pem localhost 127.0.0.1 ::1 example.com *.example.com
+    test -f $CAROOT/full-cert.pem || cat  $CAROOT/cert.pem $CAROOT/rootCA.pem > $CAROOT/full-cert.pem
+    test -f $CAROOT/rsa-private_traditional.key || openssl rsa -in $CAROOT/key.pem -traditional -out $CAROOT/rsa-private_traditional.key
+    test -f $CAROOT/private.ec.key || openssl ecparam -name prime256v1 -genkey -noout -out $CAROOT/private.ec.key
+
+    # ECDSA_DIR=$CAROOT/ecdsa-cert
+    test -d $ECDSA_DIR || mkdir $ECDSA_DIR
+    test -f $ECDSA_DIR/ecdsa.key || openssl ecparam -name prime256v1 -genkey -noout -out $ECDSA_DIR/ecdsa.key
+    test -f $ECDSA_DIR/ecdsa.crt || openssl req -new -x509 -key $ECDSA_DIR/ecdsa.key -days 825 -out $ECDSA_DIR/ecdsa.crt \
+        -subj "/CN=example.com/O=Example Org" \
+        -addext "subjectAltName=DNS:example.com,DNS:alt.example.com,IP:10.0.0.5"
+
+    # ED25519_DIR=$CAROOT/ed25519_cert
+    test -d $ED25519_DIR || mkdir $ED25519_DIR
+    test -f $ED25519_DIR/ed25519.key || openssl genpkey -algorithm Ed25519 -out $ED25519_DIR/ed25519.key
+    test -f $ED25519_DIR/ed25519.crt || openssl req -new -x509 -key $ED25519_DIR/ed25519.key -days 365 -out $ED25519_DIR/ed25519.crt \
+    -subj "/CN=example.com/O=Example Org" -addext "subjectAltName=DNS:example.com,IP:127.0.0.1"
   '';
 
   scripts.build.exec = ''
@@ -134,59 +172,59 @@
     curl "https://localhost:9443/get" -k -v
   '';
 
-  scripts.test-sample-config.exec = ''
+  scripts.test-requests-sample-config.exec = ''
     gum format "## test request with sample config"
     ./dist/https-wrench requests --config ./src/cmd/embedded/config-example.yaml
   '';
 
-  scripts.test-k3s.exec = ''
+  scripts.test-requests-k3s.exec = ''
     gum format "## test request against local k3s"
     ./dist/https-wrench requests --config ./examples/https-wrench-k3s.yaml
   '';
 
-  scripts.test-timeout.exec = ''
+  scripts.test-requests-timeout.exec = ''
     gum format "## test request timeout"
     ./dist/https-wrench requests --config ./examples/https-wrench-request-timeout.yaml | grep "Client.Timeout exceeded while awaiting headers"
   '';
 
-  scripts.test-unknown-ca.exec = ''
+  scripts.test-requests-unknown-ca.exec = ''
     gum format "## test request with unknown CA"
 
     set +o pipefail
     ./dist/https-wrench requests --config ./examples/tests-configs/unknown-ca.yaml | grep 'failed to verify certificate: x509: certificate signed by unknown authority'
   '';
 
-  scripts.test-insecure.exec = ''
-    gum format "## test insecure skip verify"
+  scripts.test-requests-insecure.exec = ''
+    gum format "## test request insecure skip verify"
     ./dist/https-wrench requests --config ./examples/tests-configs/insecure.yaml | grep 'StatusCode: 200'
   '';
 
-  scripts.test-body-regexp-match.exec = ''
-    gum format "## test body regexp match"
+  scripts.test-requests-body-regexp-match.exec = ''
+    gum format "## test request body regexp match"
     ./dist/https-wrench requests --config  ./examples/tests-configs/body-regexp-match.yaml  --ca-bundle $CAROOT/rootCA.pem | grep 'BodyRegexpMatch: true'
   '';
 
-  scripts.test-ca-bundle-file-success.exec = ''
+  scripts.test-requests-ca-bundle-file-success.exec = ''
     gum format "## test request with CA bundle file"
     ./dist/https-wrench requests --config ./examples/tests-configs/ca-bundle-200.yaml --ca-bundle $CAROOT/rootCA.pem | grep "StatusCode: 200"
   '';
 
-  scripts.test-ca-bundle-file-wrong-servername.exec = ''
+  scripts.test-requests-ca-bundle-file-wrong-servername.exec = ''
     gum format "## test request with CA bundle file and wrong host name / servername"
     ./dist/https-wrench requests --config ./examples/tests-configs/ca-bundle-wrong-servername.yaml --ca-bundle $CAROOT/rootCA.pem | grep 'tls: failed to verify certificate: x509'
   '';
 
-  scripts.test-proxy-protocol-ipv4.exec = ''
-    gum format "## test proxy protocol IPv4"
+  scripts.test-requests-proxy-protocol-ipv4.exec = ''
+    gum format "## test request proxy protocol IPv4"
     ./dist/https-wrench requests --config ./examples/tests-configs/proxy-protocol-ipv4.yaml --ca-bundle $CAROOT/rootCA.pem | grep '192.0.2.1'
   '';
 
-  scripts.test-proxy-protocol-ipv6.exec = ''
-    gum format "## test proxy protocol IPv6"
+  scripts.test-requests-proxy-protocol-ipv6.exec = ''
+    gum format "## test request proxy protocol IPv6"
     ./dist/https-wrench requests --config ./examples/tests-configs/proxy-protocol-ipv6.yaml --ca-bundle $CAROOT/rootCA.pem | grep '2001:db8::1'
   '';
 
-  scripts.test-ca-bundle-yaml.exec = ''
+  scripts.test-requests-ca-bundle-yaml.exec = ''
     gum format "## test request with CA bundle in YAML"
 
     CA_BUNDLE_YAML_TEST_FILE=./tests/https-wrench-tests-ca-bundle-string.yaml
@@ -197,6 +235,118 @@
     while IFS= read -r line; do echo  "  $line" >> $CA_BUNDLE_YAML_TEST_FILE ; done < $CAROOT/rootCA.pem
 
     ./dist/https-wrench requests --config $CA_BUNDLE_YAML_TEST_FILE | grep 'StatusCode: 200'
+  '';
+
+  scripts.test-certinfo-pkcs1-rsa-key.exec = ''
+    gum format "## test certinfo load PKCS1 RSA key"
+    ./dist/https-wrench certinfo --key-file $CAROOT/rsa-private_traditional.key | grep -E 'Type\s+RSA'
+  '';
+
+  scripts.test-certinfo-pkcs1-ec-key.exec = ''
+    gum format "## test certinfo load PKCS1 EC key"
+    ./dist/https-wrench certinfo --key-file $CAROOT/private.ec.key | grep -E 'Type\s+ECDSA'
+  '';
+
+  scripts.test-certinfo-pkcs8-rsa-key.exec = ''
+    gum format "## test certinfo load PKCS8 RSA key"
+    ./dist/https-wrench certinfo --key-file $CAROOT/key.pem | grep -E 'Type\s+RSA'
+  '';
+
+  scripts.test-certinfo-pkcs8-ecdsa-key.exec = ''
+    gum format "## test certinfo load PKCS8 ECDSA key"
+    ./dist/https-wrench certinfo --key-file $ECDSA_DIR/ecdsa.key | grep -E 'Type\s+ECDSA'
+  '';
+
+  scripts.test-certinfo-pkcs8-ed25519-key.exec = ''
+    gum format "## test certinfo load PKCS8 ED25519 key"
+    ./dist/https-wrench certinfo --key-file $ED25519_DIR/ed25519.key | grep -E 'Type\s+Ed25519'
+  '';
+
+  scripts.test-certinfo-rsa-cert.exec = ''
+    gum format "## test certinfo load RSA cert"
+    ./dist/https-wrench certinfo --cert-bundle $CAROOT/cert.pem | grep -E 'PublicKeyAlgorithm\s+RSA'
+  '';
+
+  scripts.test-certinfo-ed25519-cert.exec = ''
+    gum format "## test certinfo load ED25519 cert"
+    ./dist/https-wrench certinfo --cert-bundle $ED25519_DIR/ed25519.crt | grep -E 'PublicKeyAlgorithm\s+Ed25519'
+  '';
+
+  scripts.test-certinfo-ecdsa-cert.exec = ''
+    gum format "## test certinfo load ECDSA cert"
+    ./dist/https-wrench certinfo --cert-bundle $ECDSA_DIR/ecdsa.crt | grep -E 'PublicKeyAlgorithm\s+ECDSA'
+  '';
+
+  scripts.test-certinfo-pkcs8-rsa-key-cert.exec = ''
+    gum format "## test certinfo: PKCS8 RSA key + cert "
+    ./dist/https-wrench certinfo --key-file $CAROOT/key.pem --cert-bundle $CAROOT/cert.pem | grep 'PrivateKey match: true'
+  '';
+
+  scripts.test-certinfo-pkcs8-ecdsa-key-cert.exec = ''
+    gum format "## test certinfo: PKCS8 ECDSA key + cert"
+    ./dist/https-wrench certinfo --key-file $ECDSA_DIR/ecdsa.key --cert-bundle $CAROOT/ecdsa.crt | grep 'PrivateKey match: true'
+  '';
+
+  scripts.test-certinfo-pkcs8-ed25519-key-cert.exec = ''
+    gum format "## test certinfo: PKCS8 ED25519 key + cert"
+    ./dist/https-wrench certinfo --key-file $ED25519_DIR/ed25519.key --cert-bundle $CAROOT/ed25519.crt | grep 'PrivateKey match: true'
+  '';
+
+  scripts.test-certinfo-tlsendpoint.exec = ''
+    gum format "## test certinfo tlsEnpoint"
+    ./dist/https-wrench certinfo --tls-endpoint repo.os76.xyz:443
+  '';
+
+  scripts.test-certinfo-tlsendpoint-servername.exec = ''
+    gum format "## test certinfo tlsEnpoint servername"
+    ./dist/https-wrench certinfo --tls-endpoint repo.os76.xyz:443 --tls-servername www.os76.xyz
+  '';
+
+  scripts.test-certinfo-tlsendpoint-timeout.exec = ''
+    gum format "## test certinfo tlsEnpoint timeout"
+    set +o pipefail
+    ./dist/https-wrench certinfo --tls-endpoint repo.os76.xyz:344 2>&1 | grep timeout
+  '';
+
+  scripts.test-certinfo-tlsendpoint-malformed.exec = ''
+    gum format "## test certinfo tlsEnpoint malformed (missing port)"
+    set +o pipefail
+    ./dist/https-wrench certinfo --tls-endpoint repo.os76.xyz | grep 'missing port in address'
+  '';
+
+  scripts.test-certinfo-tlsendpoint-insecure.exec = ''
+    gum format "## test certinfo tlsEnpoint Insecure"
+    ./dist/https-wrench certinfo --tls-endpoint localhost:9443 --tls-insecure | grep 'certificate signed by unknown authority'
+  '';
+
+  scripts.test-certinfo-tlsendpoint-ca-bundle.exec = ''
+    gum format "## test certinfo tlsEnpoint + ca-bundle"
+    ./dist/https-wrench certinfo --tls-endpoint localhost:9443 --ca-bundle $CAROOT/rootCA.pem
+  '';
+
+  scripts.test-certinfo-tlsendpoint-ca-bundle-ipv4.exec = ''
+    gum format "## test certinfo IPv4 tlsEnpoint + ca-bundle"
+    ./dist/https-wrench certinfo --tls-endpoint 127.0.0.1:9443 --ca-bundle $CAROOT/rootCA.pem
+  '';
+
+  scripts.test-certinfo-tlsendpoint-ca-bundle-ipv6.exec = ''
+    gum format "## test certinfo IPV6 tlsEnpoint + ca-bundle "
+    ./dist/https-wrench certinfo --tls-endpoint [::1]:9443 --ca-bundle $CAROOT/rootCA.pem
+  '';
+
+  scripts.test-certinfo-tlsendpoint-rsa-key-cert.exec = ''
+    gum format "## test certinfo tlsEnpoint: RSA key + cert"
+    ./dist/https-wrench certinfo --tls-endpoint localhost:9443 --tls-insecure --tls-servername example.com --key-file $CAROOT/key.pem | grep 'PrivateKey match: true'
+  '';
+
+  scripts.test-certinfo-tlsendpoint-ecdsa-key-cert.exec = ''
+    gum format "## test certinfo tlsEnpoint: ECDSA key + cert"
+    ./dist/https-wrench certinfo --tls-endpoint localhost:9446 --tls-insecure --tls-servername example.com --key-file $ECDSA_DIR/ecdsa.key | grep 'PrivateKey match: true'
+  '';
+
+  scripts.test-certinfo-tlsendpoint-ed25519-key-cert.exec = ''
+    gum format "## test certinfo tlsEnpoint: ED25519 key + cert"
+    ./dist/https-wrench certinfo --tls-endpoint localhost:9445 --tls-insecure --tls-servername example.com --key-file $ED25519_DIR/ed25519.key | grep 'PrivateKey match: true'
   '';
 
   enterShell = ''
@@ -210,16 +360,37 @@
   enterTest = ''
     gum format "# Running tests"
     build
-    test-sample-config
-    test-k3s
-    test-timeout
-    test-insecure
-    test-unknown-ca
-    test-ca-bundle-file-success
-    test-ca-bundle-file-wrong-servername
-    test-proxy-protocol-ipv4
-    test-proxy-protocol-ipv6
-    test-ca-bundle-yaml
-    test-body-regexp-match
+
+    test-requests-sample-config
+    test-requests-k3s
+    test-requests-timeout
+    test-requests-insecure
+    test-requests-unknown-ca
+    test-requests-ca-bundle-file-success
+    test-requests-ca-bundle-file-wrong-servername
+    test-requests-proxy-protocol-ipv4
+    test-requests-proxy-protocol-ipv6
+    test-requests-ca-bundle-yaml
+    test-requests-body-regexp-match
+
+    test-certinfo-pkcs1-ec-key
+    test-certinfo-pkcs1-rsa-key
+    test-certinfo-pkcs8-rsa-key
+    test-certinfo-pkcs1-ec-key
+    test-certinfo-pkcs8-ecdsa-key
+    test-certinfo-rsa-cert
+    test-certinfo-ed25519-cert
+    test-certinfo-ecdsa-cert
+    test-certinfo-tlsendpoint
+    test-certinfo-tlsendpoint-servername
+    test-certinfo-tlsendpoint-timeout
+    test-certinfo-tlsendpoint-malformed
+    test-certinfo-tlsendpoint-insecure
+    test-certinfo-tlsendpoint-ca-bundle
+    test-certinfo-tlsendpoint-ca-bundle-ipv4
+    test-certinfo-tlsendpoint-ca-bundle-ipv6
+    test-certinfo-tlsendpoint-rsa-key-cert
+    test-certinfo-tlsendpoint-ecdsa-key-cert
+    test-certinfo-tlsendpoint-ed25519-key-cert
   '';
 }
