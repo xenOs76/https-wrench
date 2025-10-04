06790e0d7e32675b049b4d2a8ad53db7c3882540
Zeno Belli <xeno@os76.xyz>
2025-10-04 14:27:40 +0200

    build: update go dependencies before building

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 test -d dist || mkdir dist
 
 cd src || exit
+
+go get -u
+
 APP_VERSION=$(git describe --tags || echo '0.0.0') &&
     GO_MODULE_NAME=$(grep module go.mod | awk '{ print $2 }') &&
     CGO_ENABLED=0 GOOS=linux go build -o ../dist/https-wrench -ldflags "-X $GO_MODULE_NAME/cmd.version=$APP_VERSION" main.go
