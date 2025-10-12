10edcaec1a38d2e95173fac2959e4cae134243b7
Zeno Belli <xeno@os76.xyz>
2025-10-12 19:27:52 +0200

    chore: delete unused files

--- dockerImageBuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t https-wrench . || exit 1
-
-DOCKER_RUN="docker run --rm https-wrench --help"
-echo -e "Running the following command: $DOCKER_RUN\n"
-$DOCKER_RUN
--- updatePackages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-nix flake update
-
-cd src || exit
-go get -u
-go mod tidy
