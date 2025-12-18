38d80a309b73f2b30b62612eadf2f39fb0115309
Zeno Belli <xeno@os76.xyz>
2025-12-18 18:34:09 +0100

    ci: add devenv test to code checks

--- devenv.nix
+++ devenv.nix
@@ -148,6 +148,14 @@
     enable = true;
   };
 
+  tasks."web:refreshCertsBeforeNginxStart" = {
+    exec = ''
+      test -d ${config.env.DEVENV_ROOT}/tests && rm -rf ${config.env.DEVENV_ROOT}/tests
+      create-certs
+    '';
+    before = ["devenv:processes:nginx"];
+  };
+
   scripts.hello.exec = ''
     gum format "# Devenv shell"
   '';
@@ -615,7 +623,7 @@
     # update-go-deps
     build
 
-    run-go-tests
+    #run-go-tests
 
     test-cmd-root-version
     test-cmd-requests-version
